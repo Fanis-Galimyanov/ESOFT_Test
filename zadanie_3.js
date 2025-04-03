7e319e949cfac023f30ee35fe5d23ba5722c182c
Fanis-Galimyanov <fanisgalimyanov@gmail.com>
2025-04-03 23:50:00 +0300

    The third task has been completed

--- zadanie_3.js
+++ zadanie_3.js
@@ -1,10 +1,12 @@
 const start = document.getElementById('start');
-skobrki = ['(',')','{','}','[',']'];
+let skobrki = ['(',')','{','}','[',']'];
+let skobrki_otkrivayuchie = ['(','{','['];
+let skobrki_zakrivayuchie = [')','}',']'];
 
 let vvedni_tolko_skobki = true;
 
 start.onclick = function() {
-    posledovatelnost = document.getElementById('posledovatelnost').value;
+    let posledovatelnost = document.getElementById('posledovatelnost').value;
 
     let array_posledovatelnost = posledovatelnost.split('');
 
@@ -17,24 +19,47 @@ start.onclick = function() {
         
         }
         
-    
-    alert(vvedni_tolko_skobki);
-    
+    if(vvedni_tolko_skobki){
+        let stack = [];
+        let pravilnaya_skobchnaya_posledovatelnost = true;
+        for(const element_posledovatelnosty of array_posledovatelnost){
+            if(skobrki_otkrivayuchie.includes(element_posledovatelnosty)){
+                stack.push(element_posledovatelnosty);
+            }
+            else if(skobrki_zakrivayuchie.includes(element_posledovatelnosty)){
+                
+                if(stack.length == 0){
+                    pravilnaya_skobchnaya_posledovatelnost = false;
+                    break;
+                }
+                
+                let open_bracket = stack.pop();
 
+                if(open_bracket == '(' && element_posledovatelnosty == ')'){
+                    continue;
+                }
 
+                if(open_bracket == '[' && element_posledovatelnosty == ']'){
+                    continue;
+                }
+
+                if(open_bracket == '{' && element_posledovatelnosty == '}'){
+                    continue;
+                }
+
+                pravilnaya_skobchnaya_posledovatelnost = false;
+                break;
+            }
+
+        }
+        if(pravilnaya_skobchnaya_posledovatelnost && stack.length == 0){
+            alert('Введена правельная скобочная последовательность!');
+            }
+        else{
+            alert('Не правельная скобочная последовательность!!!');
+        }
+    }
+
+    vvedni_tolko_skobki = true;
 
   };
-
-
-/*String = '{}()[';
-
-let stek = [];
-stek.push('{');
-stek.push('[');
-stek.push(']');
-stek.push('}');
-
-console.log(stek);
-console.log(stek.pop());
-console.log(stek.pop());
-console.log(stek);*/
